448999afd40e819da081b46d88d54bd9b06dfe3d
Erik Lindberg <e.lindberg@gmail.com>
2021-01-15 22:23:14 -0800

    Can now do duplication of tokens

--- src/renderer/components/LandingPage/DirectoryListing.vue
+++ src/renderer/components/LandingPage/DirectoryListing.vue
@@ -269,7 +269,7 @@ export default {
         }
         // Convert to desired format
         let finalFile = await this.convert(fileName, this.selected[i])
-        let trackTitle = this.titleFormat.replace('%title%', this.selected[i].title).replace('%artist%', this.selected[i].artist).replace('%trackno%', this.selected[i].trackNo)
+        let trackTitle = this.titleFormat.replace(/%title%/g, this.selected[i].title).replace(/%artist%/g, this.selected[i].artist).replace(/%trackno%/g, this.selected[i].trackNo)
         console.log('Title: ' + trackTitle)
         console.log('Conversion Complete: ' + finalFile)
         await this.sendToPlayer(finalFile, trackTitle)
